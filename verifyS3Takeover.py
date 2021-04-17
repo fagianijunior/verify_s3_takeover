43bc1b4192b8ae64e6b9ab3e561bd4a7db59586e
Carlos Fagiani Junior <fagianijunior@gmail.com>
2021-04-16 23:42:19 -0300

    Refactor

--- verifyS3Takeover.py
+++ verifyS3Takeover.py
@@ -1,52 +1,83 @@
 # Author: fagianijunior <fagianijunior@gmail.com>
 # References: https://gupta-bless.medium.com/exploiting-subdomain-takeover-on-s3-6115730d01d7
-# References: 
 
 import sys
 import boto3
 import re
-import cloudflare
+import CloudFlare
 
-accounts = ['tink']
-if accounts.count <= 0:
-    "Add some account name in account array"
-else:
+def main():
+    accounts = ['tink']
+
+    if not accounts:
+        print("Add some account name in account array\nExiting...")
+        return 0
+
     for account in accounts:
-        sys.stdout.write("\033[1;36m")
-        print('|----------------------------------------------------|')
-        print('| CHECKING ACCOUNT: ' + account)
-        print('|----------------------------------------------------|')
-        sys.stdout.write("\033[0;0m")
+        accountBanner(account)
+        verify(account)
 
-        # define o perfil
-        session = boto3.Session(profile_name=account)
+def listBuckets(session):
+    s3        = session.client('s3')
+    s3Buckets = s3.list_buckets()
+    buckets   = []
+    
+    for bucket in s3Buckets['Buckets']:
+        buckets.append(bucket['Name'])
 
-        route53 = session.client('route53')
-        hostedZones = route53.list_hosted_zones()
-        s3WebsiteRegex = '(.s3-website-)(us|af|ap|ca|eu|me|sa)(-)(east|west|south|northeast|southeast|central|north)(-[1-3])(.amazonaws.com)?(.)'
+    return buckets
 
-        s3 = session.client('s3')
-        s3Buckets = s3.list_buckets()
+def listHostedZones(session):
+    route53 = session.client('route53')
 
-        buckets = []
-        for bucket in s3Buckets['Buckets']:
-            buckets.append(bucket['Name'])
+    hostedZones = route53.list_hosted_zones()
 
-        for hostedZone in hostedZones['HostedZones']:
-            print('verificando: ' + hostedZone['Name'])
-            records = route53.list_resource_record_sets(
-                HostedZoneId=hostedZone['Id']
-            )
-            for record in records['ResourceRecordSets']:
-                if record['Type'] in ['CNAME']:
-                    if re.search(s3WebsiteRegex, record['ResourceRecords'][0]['Value']):
-                        print(record['Name'] + ": " + record['ResourceRecords'][0]['Value'])
-                        
-                        recordValue = re.sub(s3WebsiteRegex, '', record['ResourceRecords'][0]['Value'])
-                        if recordValue in buckets:
-                            sys.stdout.write("\033[0;32m")
-                            print('OK ' + recordValue)
-                        else:
-                            sys.stdout.write("\033[1;31m")
-                            print('WARNING ' + recordValue)
-        sys.stdout.write("\033[0;0m")
+    return hostedZones
+
+def listRecords(session, hostedZone):
+    route53 = session.client('route53')
+    records = route53.list_resource_record_sets(
+            HostedZoneId=hostedZone['Id']
+        )
+    return records
+
+def verifyRecord(record):
+    s3WebsiteRegex  = '(.s3-website-)(us|af|ap|ca|eu|me|sa)(-)(east|west|south|northeast|southeast|central|north)(-[1-3])(.amazonaws.com)?(.)'
+
+    if record['Type'] in ['CNAME']:
+        if re.search(s3WebsiteRegex, record['ResourceRecords'][0]['Value']):
+            print(record['Name'] + ": " + record['ResourceRecords'][0]['Value'])
+            
+            recordValue = re.sub(s3WebsiteRegex, '', record['ResourceRecords'][0]['Value'])
+            if recordValue in buckets:
+                sys.stdout.write("\033[0;32m")
+                print('OK ' + recordValue)
+            else:
+                sys.stdout.write("\033[1;31m")
+                print('WARNING ' + recordValue)
+
+
+def accountBanner(account):
+    sys.stdout.write("\033[1;36m")
+    print('|----------------------------------------------------|')
+    print('| CHECKING ACCOUNT: ' + account)
+    print('|----------------------------------------------------|')
+    sys.stdout.write("\033[0;0m")
+
+
+def verify(account):
+    # define o perfil
+    session         = boto3.Session(profile_name=account)
+    buckets         = listBuckets(session)
+    hostedZones     = listHostedZones(session)
+
+    for hostedZone in hostedZones['HostedZones']:
+        print('verificando: ' + hostedZone['Name'])
+        records = listRecords(session, hostedZone)
+        for record in records['ResourceRecordSets']:
+            verifyRecord(record)
+    sys.stdout.write("\033[0;0m")
+
+
+if __name__ == '__main__':
+    main()
